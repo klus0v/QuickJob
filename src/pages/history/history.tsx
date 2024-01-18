7eceedb0e3305bbc9d184d12a5df3bf94ef9b1a7
Ural Guru <gizzatullinu@gmail.com>
2024-01-18 15:45:23 +0500

    RefactoringHistory

--- src/pages/history/history.tsx
+++ src/pages/history/history.tsx
@@ -1,7 +1,6 @@
 import styles from './history.module.css';
 import '@fontsource-variable/montserrat';
 import Layout from '../../components/layout/layout';
-import { Link } from 'react-router-dom';
 import VacancyCard from '../../components/cards/vacancyCard/vacancyCard';
 import { useEffect, useState } from 'react';
 import cn from 'classnames';
@@ -10,15 +9,19 @@ import AuthPopup from '../../components/popups/auth/auth';
 import { getHistoryOrdersThunk } from '../../store/slices/history.slice';
 
 function History() {
-    const [flag, setFlag] = useState(true);
+    const [isCustomer, setCustomer] = useState(true);
 
     const isAuth = useAppSelector(state => state.auth.isAuth);
     const [isOpenAuthPOpup, setOpenAuthPOpup] = useState(!isAuth);
 
     const dispatch = useAppDispatch();
+
     useEffect(() => {
-        dispatch(getHistoryOrdersThunk(flag ? 'Customer' : 'Worker'));
-    }, [flag]);
+        isAuth &&
+            dispatch(getHistoryOrdersThunk(isCustomer ? 'Customer' : 'Worker'));
+    }, [isCustomer, isAuth]);
+
+    const historyOrders = useAppSelector(state => state.history.foundItems);
 
     return (
         <div className={styles.container}>
@@ -30,33 +33,31 @@ function History() {
 
             <div className={styles.main}>
                 <nav className={styles.links}>
-                    <Link
-                        to="#"
+                    <div
                         className={cn(
                             styles.backPageLink,
-                            flag && styles.active,
+                            isCustomer && styles.active,
                         )}
-                        onClick={() => setFlag(true)}
+                        onClick={() => setCustomer(true)}
                     >
                         Я заказчик
-                    </Link>
-                    <Link
-                        to="#"
+                    </div>
+                    <div
                         className={cn(
                             styles.backPageLink,
-                            !flag && styles.active,
+                            !isCustomer && styles.active,
                         )}
-                        onClick={() => setFlag(false)}
+                        onClick={() => setCustomer(false)}
                     >
                         Я исполнитель
-                    </Link>
+                    </div>
                 </nav>
 
                 <div className={styles.mainCard}>
-                    <VacancyCard />
-                    <VacancyCard />
-                    <VacancyCard />
-                    <VacancyCard />
+                    {isAuth &&
+                        historyOrders.map(order => (
+                            <VacancyCard {...order} key={order.id} />
+                        ))}
                 </div>
             </div>
             <AuthPopup isOpen={isOpenAuthPOpup} setIsOpen={setOpenAuthPOpup} />
