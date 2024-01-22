dd2f6d399104752e7856aeb1f5405fda14f8c040
Danil <72307503+klus0v@users.noreply.github.com>
2024-01-22 21:30:27 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
     plugins: [react()],
     server: {
+        hmr: {
+                host: '0.0.0.0',
+                port: 5173,
+            },
         proxy: {
             '/api': {
                 target: 'http://51.250.93.99:5555',
